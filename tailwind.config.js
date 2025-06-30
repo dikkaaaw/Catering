109cdf29164e70215fde61d33c8c9fc6bdb43a91
Dika Wicaksono <dhkwck00@gmail.com>
2025-06-30 13:06:34 +0700

    feat: add additional content paths and custom text utilities to Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './App.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
@@ -17,5 +21,28 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-h1': {
+          fontFamily: 'SF-Pro-Display-Semibold',
+          fontSize: '34px',
+          color: '#010F07',
+        },
+        '.text-body': {
+          fontFamily: 'SF-Pro-Display-Regular',
+          fontSize: '18px',
+          color: '#868686',
+          lineHeight: '24px',
+        },
+        '.text-headline': {
+          fontFamily: 'SF-Pro-Display-Bold',
+          fontSize: '30px',
+          color: '#010F07',
+          lineHeight: '38px',
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
 };
