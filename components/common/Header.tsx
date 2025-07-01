744edf575e8a1fec2478851591615c63fe4afdc2
Dika Wicaksono <dhkwck00@gmail.com>
2025-07-01 14:48:12 +0700

    refactor: enhance Header component to support close icon functionality

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -4,27 +4,34 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 interface HeaderProps {
   title?: string;
   arrowIcon?: boolean;
+  closeIcon?: boolean;
   onBackPress?: () => void;
 }
 
-const Header = ({ title, arrowIcon, onBackPress }: HeaderProps) => {
+const Header = ({ title, arrowIcon, closeIcon, onBackPress }: HeaderProps) => {
+  const renderLeftIcon = () => {
+    if (arrowIcon) {
+      return (
+        <TouchableOpacity onPress={onBackPress}>
+          <Image style={{ width: 24, height: 24 }} source={icons.back} />
+        </TouchableOpacity>
+      );
+    }
+    if (closeIcon) {
+      return (
+        <TouchableOpacity onPress={onBackPress}>
+          <Image style={{ width: 16, height: 16 }} source={icons.close} />
+        </TouchableOpacity>
+      );
+    }
+    return <View style={{ width: 24 }} />;
+  };
+
   return (
     <View className="mt-2 flex flex-row items-center justify-between px-6">
-      {arrowIcon ? (
-        <TouchableOpacity onPress={onBackPress}>
-          <Image
-            style={{
-              width: 24,
-              height: 24,
-            }}
-            source={icons.back}
-          />
-        </TouchableOpacity>
-      ) : (
-        <View style={{ width: 24 }} />
-      )}
+      {renderLeftIcon()}
       <View className="flex-1 items-center">
-        <Text className="font-sf-bold text-center text-[18px] tracking-wider text-black">
+        <Text className="text-center font-sf-bold text-[18px] tracking-wider text-black">
           {title}
         </Text>
       </View>
